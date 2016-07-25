9e7cbc7f7316ffe70f297ad1d84bff582f1392cf
Tobias Ponce <Kymera@Tobiass-MacBook-Pro.local>
2016-07-24 23:56:33 -0300

    first arduino version

---
+++ blink_v1/blink_v1.ino
@@ -0,0 +1,22 @@
+int porta_rele1 = 5;
+int porta_rele2 = 6;
+int porta_rele3 = 7;
+
+int upsideDown[3];
+
+void setup(){
+  int a = pinMode(porta_rele1, OUTPUT);
+  int b = pinMode(porta_rele2, OUTPUT);
+  int c = pinMode(porta_rele3, OUTPUT);
+  
+  upsideDown[] = a;
+  upsideDown[] = b;
+  upsideDown[] = c;
+}
+
+void loop(){
+  strangerMessage('abc');
+}
+
+
+
